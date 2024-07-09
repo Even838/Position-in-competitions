606506b5a8127a1db6c2e008ee1240aa699b1c92
Even-dev <samuca1234.oficial@gmail.com>
2024-07-09 20:21:21 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int position = 1;
+        
+        switch (position) {
+            case 1:
+                System.out.println("fist");
+                break;
+            case 2:
+                System.out.println("second");
+                break;
+            case 3:
+                System.out.println("third");
+                break;
+            default:
+                System.out.println("");
+        }
+    }
+}
